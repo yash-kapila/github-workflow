74536458430ec3cdf2ef3388c79df714cb453f15
Yash Kapila <yash.kapila18@gmail.com>
2020-07-03 22:19:05 +0200

    fix failing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'github-workflow'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('github');
+    expect(app.title).toEqual('github-workflow');
   });
 
   it('should render title', () => {
